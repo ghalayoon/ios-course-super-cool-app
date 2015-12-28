d7d99ee48f797197a5d71b64e3b7c1196a2f4437
Yousef Ghalayini <ghalayini.yousef@gmail.com>
2015-12-28 13:35:22 -0500

    Initial Commit

---
+++ supercoolagain/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  supercoolagain
+//
+//  Created by Yousef Ghalayini on 2015-12-28.
+//  Copyright © 2015 Yousef Ghalayini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
